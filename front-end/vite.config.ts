b7f3c5475a39d6b422f1c46bcde5b9eee31e9ab2
Probir Sarkar <Probirsarkar855@gmail.com>
2024-07-23 00:50:15 +0530

    remove pnpm lock

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+      "@": path.resolve(__dirname, "src")
+    }
+  }
 });
