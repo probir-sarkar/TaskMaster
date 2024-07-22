69fbc890c4298b4b00eaa3da241940ad7eea7519
Probir Sarkar <Probirsarkar855@gmail.com>
2024-07-23 00:54:18 +0530

    remove swc

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
