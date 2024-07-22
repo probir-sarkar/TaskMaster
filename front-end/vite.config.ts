cbf8867d26ae02e5315e5ac78e330fef29adce22
Probir Sarkar <Probirsarkar855@gmail.com>
2024-07-23 00:53:48 +0530

    path update

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "node:path";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
