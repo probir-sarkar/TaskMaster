ac7ed1b093374add03b911edb12658984c04eb16
Probir Sarkar <Probirsarkar855@gmail.com>
2024-07-23 08:56:04 +0530

    jwt bug fix

--- back-end/src/utils/jwt.ts
+++ back-end/src/utils/jwt.ts
@@ -7,13 +7,14 @@ export function verifyToken(token: string): object | string {
 
 export function generateToken(payload: object | string, expiresIn: string = "1d"): string {
   return jwt.sign(payload, env.jwtSecret, {
-    expiresIn: expiresIn,
+    expiresIn: expiresIn
   });
 }
 
 export function isTokenTimeExpired(token: string): boolean {
   try {
-    const decoded = jwt.decode(token, { complete: true });
+    const decoded = jwt.decode(token, { complete: true }) as jwt.JwtPayload;
+    if (!decoded) return true;
     const dateNow = new Date();
     const dateExpire = new Date(decoded.payload.exp * 1000);
     return dateNow > dateExpire;
