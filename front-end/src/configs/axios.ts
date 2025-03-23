d4c33b587be0008170f92726e18854ae112b1e38
Probir Sarkar <probirsarkar855@gmail.com>
2025-03-23 13:35:39 +0530

    Increase timeout of axios request

--- front-end/src/configs/axios.ts
+++ front-end/src/configs/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // Replace with your API base URL
-  timeout: 1000,
+  timeout: 10000,
   headers: { "Content-Type": "application/json" },
   withCredentials: true // This sends cookies with cross-site requests
 });
