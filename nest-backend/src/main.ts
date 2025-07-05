3d825de8e7590a30a620f99c0e7e1fab008b7bae
Probir Sarkar <probirsarkar855@gmail.com>
2025-07-05 18:02:14 +0530

    exclude base path

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import cookieParser from "cookie-parser";
-import { RequestMethod, ValidationPipe } from "@nestjs/common";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,9 +16,6 @@ async function bootstrap() {
       whitelist: true, // strips unknown properties
     }),
   );
-  app.setGlobalPrefix("api", {
-    exclude: [{ path: "health", method: RequestMethod.GET }],
-  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
