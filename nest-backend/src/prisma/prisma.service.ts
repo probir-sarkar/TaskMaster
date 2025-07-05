4c7b590aedd4814afa085255e14cbcf6efe1ddc5
Probir Sarkar <probirsarkar855@gmail.com>
2025-07-05 17:54:25 +0530

    prisma issue fix

--- nest-backend/src/prisma/prisma.service.ts
+++ nest-backend/src/prisma/prisma.service.ts
@@ -1,6 +1,6 @@
 // src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
-import { PrismaClient } from "generated/prisma";
+import { PrismaClient } from "../../generated/prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
